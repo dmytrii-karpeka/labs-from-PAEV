9342b6119e7af3ae893d947645b2c246040cdb9f
dmytrii-karpeka <dmitriy.karpeka@gmail.com>
2022-10-04 20:36:39 +0300

    main mechanic is working! can send builetens safely

--- start.js
+++ start.js
@@ -1,30 +1,85 @@
+const crypto = require('crypto');
+const { ElGamal, Alphabet } = require('./elgamal.js');
 
 
 class Voter {
-    #eg;
-    #voteFor;
-    constructor(name, age, theirCVK) {
+    #theirKeys;
+    #theirBuileten;
+    #hashedBuileten;
+        constructor(name, age, theirCVK, message) {
         this.name = name;
         this.age = age;
         this.theirCVK = theirCVK;
-        this.#eg = "";
-        this.#voteFor = "";
+        this.message = message;
+        this.#theirKeys = { };
+        this.#theirBuileten = {
+            // signa: { signature and public key of RSA sign } OBJECT
+            // buileten: elGamalized with given public key from CVK } STRING
+        };
+        this.#hashedBuileten = "";
     }
 
+    // get getCVKName() {
+    //     return this.theirCVK
+    // }
 
-}
+    givePublicKey() {
+        return this.#theirKeys.publicRSAKey;
+    }
 
-class Candidate extends Voter {
-    constructor(name, key) {
-        super(name, key);
+    #generatePublicKey() {
+        const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", { modulusLength: 2048});
+        this.#theirKeys.publicRSAKey = publicKey;
+        this.#theirKeys.privateRSAKey = privateKey;
+        // console.log(this.#theirKeys.publicRSAKey);
+        // console.log(this.#theirKeys.privateRSAKey);  
+    }
+
+    #signBuileten() {
+        this.#generatePublicKey();
+        const verifiableData = this.message;
+        const signature = crypto.sign("sha256", Buffer.from(verifiableData), {
+            key: this.#theirKeys.privateRSAKey,
+            padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+        });
+        // console.log(signature.toString("base64"));
+
+        const isVerified = crypto.verify(
+            "sha256",
+            Buffer.from(verifiableData),
+            {
+              key: this.#theirKeys.publicRSAKey,
+              padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+            },
+            signature
+          );
+          
+          this.#theirBuileten.signa = {
+            theirSign: signature,
+            theirPKey: this.#theirKeys.publicRSAKey
+          }
+          // isVerified should be `true` if the signature is valid
+          console.log("signature verified: ", isVerified);
+    }
+
+    #elGamalizeBuileten(channels) {
+        const hashedMessage = channels.ch1.encrypt(this.message, channels.ch2.pubKey);
+        this.#hashedBuileten = hashedMessage;
+        this.#theirBuileten.buileten = this.#hashedBuileten;
+    }
+
+    sendBuileten(channels) {
+        this.#signBuileten();
+        this.#elGamalizeBuileten(channels);
+        return this.#theirBuileten;
     }
 }
+
 
 class CVK {
     #listOfVoters;
-    #privateKey1;
-    #crypto;
-    constructor(listOfCandidates) {
+    constructor(nameOfCVK) {
+        this.nameOfCVK = nameOfCVK;
         this.listOfCandidates = {
             "Candidate1": {
                 name: "Ilon Musk, who support Ukraine",
@@ -38,107 +93,128 @@ class CVK {
         this.#listOfVoters = {
             "voter1": {
                 name: "Maria Andrushko",
-                age: 18
+                age: 18,
+                g: 19
             },
             "voter2": {
                 name: "Pavlo Stonkevych",
-                age: 21
+                age: 21,
+                g: 27
             },
             "voter3": {
                 name: "Yakym Galayda",
-                age: 19
+                age: 19,
+                g: 26
             },
             "voter4": {
                 name: "Oksana Styslo",
-                age: 42
+                age: 42,
+                g: 15
             },
             "voter5": {
                 name: "Vasylyna Prybylo",
-                age: 67
+                age: 67,
+                g: 18
             },
             "voter6": {
                 name: "Vadym Deter",
-                age: 31
+                age: 31,
+                g: 24
             },
             "voter7": {
                 name: "Anna Yaroslavna",
-                age: 990
+                age: 990,
+                g: 11
             },
             "voter8": {
                 name: "Dmytrii Karpeka",
-                age: 20
+                age: 20,
+                g: 62
             }
         };
-        this.#crypto = require('crypto');
         // this.#privateKey1 = "";
         // this.publicKey1 = this.#generateKeysForRSASign;
     }
 
-    #generateKeysForRSASign() {
-        const { publicKey, privateKey } = this.#crypto.generateKeyPairSync("rsa", { modulusLength: 2048});
-        this.privateKey1 = privateKey;
-        // const {publicKey, privateKey} = this.crypto.generateKeyPairSync("rsa", {modulusLength: 2048});
-        // this.publicKey = publicKey;
-        // this.#privateKey = privateKey;
-        return publicKey;
-        // return this.#crypto.generateKeySync('aes', {length: 128});
-        // console.log(this.publicKey1.export().toString("hex"));
-        // const pK = this.#crypto.generateKeySync('aes', {length: 128});
-    }
+    // #generateKeysForRSASign() {
+    //     const { publicKey, privateKey } = this.#crypto.generateKeyPairSync("rsa", { modulusLength: 2048});
+    //     this.privateKey1 = privateKey;
+    //     return publicKey;
+    // }
     
 
     givePublicKey(voterNumb) {
-        const { publicKey, privateKey } = this.#crypto.generateKeyPairSync("rsa", { modulusLength: 2048});
-        this.#listOfVoters[voterNumb].publicK = publicKey;
-        this.#listOfVoters[voterNumb].secretKey = privateKey;
-        console.log(this.#listOfVoters[voterNumb].name, this.#listOfVoters[voterNumb].secretKey);
-        console.log(this.#listOfVoters[voterNumb].publicK);
-        return this.#listOfVoters[voterNumb].publicK;
+        // const { publicKey, privateKey } = this.#crypto.generateKeyPairSync("rsa", { modulusLength: 2048});
+        // this.#listOfVoters[voterNumb].publicK = publicKey;
+        // this.#listOfVoters[voterNumb].secretKey = privateKey;
+        // console.log(this.#listOfVoters[voterNumb].name, this.#listOfVoters[voterNumb].secretKey);
+        // console.log(this.#listOfVoters[voterNumb].publicK);
+        // return this.#listOfVoters[voterNumb].publicK;
+        let Ch1 = ElGamal(Alphabet, this.#listOfVoters[voterNumb].g);
+        let Ch2 = ElGamal(Alphabet, this.#listOfVoters[voterNumb].g);
+        this.#listOfVoters[voterNumb].channels = {
+            ch1: Ch1,
+            ch2: Ch2
+        }
+        return this.#listOfVoters[voterNumb].channels;
     }
 
+    getBuileten(buileten) {
+        // const decryptedData = this.#crypto.privateDecrypt({
+        //     key: this.#listOfVoters[voterNumb].secretKey,
+        //     padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+        //     oaepHash: "sha256",
+        // },
+        // buileten);
+        // this.#listOfVoters[voterNumb].voteFor = decryptedData.toString();
+        // console.log("decrypted data:", decryptedData.toString());
+        
+        // let channelCVK = ElGamal(Alphabet, )
+        const encryptedMessage = buileten.buileten;
+        this.#listOfVoters.find((v, i) => {
+
+        })
 
 
-    getBuileten(buileten, voterNumb) {
-        const decryptedData = this.#crypto.privateDecrypt({
-            key: this.#listOfVoters[voterNumb].secretKey,
-            padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-            oaepHash: "sha256",
-        },
-        buileten);
-        this.#listOfVoters[voterNumb].voteFor = decryptedData.toString();
-        console.log("decrypted data:", decryptedData.toString());
+        return buileten;
     }
 }
 
 
 const data = "Repto Potroshenko";
-const CVK1 = new CVK(['Repto', 'Potroshenko']);
-console.log(CVK1.givePublicKey("voter1"));
+let CVK1 = new CVK("CVK#1");
 
-let crypto = require('crypto');
+let voter1 = new Voter("Anna Yaroslavna", 990, "CVK#1", "Repto Potroshenko");
+console.log(CVK1.getBuileten(voter1.sendBuileten(CVK1.givePublicKey("voter1"))));
 
-const encryptedData = crypto.publicEncrypt(
-    {
-        key: CVK1.givePublicKey("voter1"),
-        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-        oaepHash: "sha256",
-    },
-    Buffer.from(data)
-);
+// voter1.givePublicKey();
+// console.log(voter1.sendBuileten());
 
 
 
-console.log("encrypted repto: ", encryptedData.toString("base64"));
-CVK1.getBuileten(encryptedData, "voter1");
 
-// const ElGamal = require('elgamal');
-// const secret = '12323131231';
-// const eg = ElGamal.generateAsync();
-// const encrypted = eg.encryptAsync(secret);
-// const decrypted = eg.decryptAsync(secret);
 
-// console.log(decrypted.toString);
 
-// let crypto2 = require('crypto');
-// const pK1 = crypto2.generateKeySync('aes', {length: 128});
-// console.log(pK1);
+
+
+
+
+
+
+// console.log(CVK1.givePublicKey("voter1"));
+
+// let crypto = require('crypto');
+
+// const encryptedData = crypto.publicEncrypt(
+//     {
+//         key: CVK1.givePublicKey("voter1"),
+//         padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+//         oaepHash: "sha256",
+//     },
+//     Buffer.from(data)
+// );
+
+
+
+// console.log("encrypted repto: ", encryptedData.toString("base64"));
+// CVK1.getBuileten(encryptedData, "voter1");
