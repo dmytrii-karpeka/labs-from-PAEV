f70581540ab0743b8eafb40cd0921cdb3c39ce83
dmytrii-karpeka <dmitriy.karpeka@gmail.com>
2022-10-04 23:33:52 +0300

    stable version without deciphering of vote.message and counting votes for candidates

--- start.js
+++ start.js
@@ -19,10 +19,6 @@ class Voter {
         this.#hashedBuileten = "";
     }
 
-    // get getCVKName() {
-    //     return this.theirCVK
-    // }
-
     givePublicKey() {
         return this.#theirKeys.publicRSAKey;
     }
@@ -37,29 +33,29 @@ class Voter {
 
     #signBuileten() {
         this.#generatePublicKey();
-        const verifiableData = this.message;
+        const verifiableData = this.name;
         const signature = crypto.sign("sha256", Buffer.from(verifiableData), {
             key: this.#theirKeys.privateRSAKey,
             padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
         });
         // console.log(signature.toString("base64"));
 
-        const isVerified = crypto.verify(
-            "sha256",
-            Buffer.from(verifiableData),
-            {
-              key: this.#theirKeys.publicRSAKey,
-              padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
-            },
-            signature
-          );
+        // const isVerified = crypto.verify(
+        //     "sha256",
+        //     Buffer.from(verifiableData),
+        //     {
+        //       key: this.#theirKeys.publicRSAKey,
+        //       padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+        //     },
+        //     signature
+        //   );
           
           this.#theirBuileten.signa = {
             theirSign: signature,
             theirPKey: this.#theirKeys.publicRSAKey
           }
           // isVerified should be `true` if the signature is valid
-          console.log("signature verified: ", isVerified);
+        //   console.log("signature verified: ", isVerified);
     }
 
     #elGamalizeBuileten(channels) {
@@ -80,58 +76,82 @@ class CVK {
     #listOfVoters;
     constructor(nameOfCVK) {
         this.nameOfCVK = nameOfCVK;
-        this.listOfCandidates = {
-            "Candidate1": {
+        this.listOfCandidates = [
+            {
                 name: "Ilon Musk, who support Ukraine",
                 votes: 0
             },
-            "Candidate2": {
+            {
                 name: "Ilon Musk, who support russia",
                 votes: 0
             }
-        };
-        this.#listOfVoters = {
-            "voter1": {
+        ];
+        this.#listOfVoters = [
+            {
                 name: "Maria Andrushko",
                 age: 18,
-                g: 19
+                channels: {
+                    ch1: ElGamal(Alphabet, 19),
+                    ch2: ElGamal(Alphabet, 19)
+                }
             },
-            "voter2": {
+            {
                 name: "Pavlo Stonkevych",
                 age: 21,
-                g: 27
+                channels: {
+                    ch1: ElGamal(Alphabet, 27),
+                    ch2: ElGamal(Alphabet, 27)
+                }
             },
-            "voter3": {
+            {
                 name: "Yakym Galayda",
                 age: 19,
-                g: 26
+                channels: {
+                    ch1: ElGamal(Alphabet, 26),
+                    ch2: ElGamal(Alphabet, 26)
+                }
             },
-            "voter4": {
+            {
                 name: "Oksana Styslo",
                 age: 42,
-                g: 15
+                channels: {
+                    ch1: ElGamal(Alphabet, 15),
+                    ch2: ElGamal(Alphabet, 15)
+                }
             },
-            "voter5": {
+            {
                 name: "Vasylyna Prybylo",
                 age: 67,
-                g: 18
+                channels: {
+                    ch1: ElGamal(Alphabet, 18),
+                    ch2: ElGamal(Alphabet, 18)
+                }
             },
-            "voter6": {
-                name: "Vadym Deter",
+            {
+                name: "Vadym Detec",
                 age: 31,
-                g: 24
+                channels: {
+                    ch1: ElGamal(Alphabet, 24),
+                    ch2: ElGamal(Alphabet, 24)
+                }
             },
-            "voter7": {
+            {
                 name: "Anna Yaroslavna",
                 age: 990,
-                g: 11
+                channels: {
+                    ch1: ElGamal(Alphabet, 11),
+                    ch2: ElGamal(Alphabet, 11)
+                }
             },
-            "voter8": {
+            {
                 name: "Dmytrii Karpeka",
                 age: 20,
-                g: 62
-            }
-        };
+                channels: {
+                    ch1: ElGamal(Alphabet, 62),
+                    ch2: ElGamal(Alphabet, 62)
+                }
+            }];
+        
         // this.#privateKey1 = "";
         // this.publicKey1 = this.#generateKeysForRSASign;
     }
@@ -143,21 +163,25 @@ class CVK {
     // }
     
 
-    givePublicKey(voterNumb) {
+    givePublicKey(voterName) {
         // const { publicKey, privateKey } = this.#crypto.generateKeyPairSync("rsa", { modulusLength: 2048});
         // this.#listOfVoters[voterNumb].publicK = publicKey;
         // this.#listOfVoters[voterNumb].secretKey = privateKey;
         // console.log(this.#listOfVoters[voterNumb].name, this.#listOfVoters[voterNumb].secretKey);
         // console.log(this.#listOfVoters[voterNumb].publicK);
         // return this.#listOfVoters[voterNumb].publicK;
-        let Ch1 = ElGamal(Alphabet, this.#listOfVoters[voterNumb].g);
-        let Ch2 = ElGamal(Alphabet, this.#listOfVoters[voterNumb].g);
-        this.#listOfVoters[voterNumb].channels = {
-            ch1: Ch1,
-            ch2: Ch2
+        let indexOfName;
+        const canFindName = this.#listOfVoters.find((v, i) => {
+            indexOfName = i;
+            return v.name === voterName;
+        });
+        if (canFindName) {
+            return this.#listOfVoters[indexOfName].channels;
+        } else {
+            console.warn(`Sorry, but you cannot vote at this CVK, ${voterName}`);
+            return 0;
         }
-        return this.#listOfVoters[voterNumb].channels;
-    }
+    };
 
     getBuileten(buileten) {
         // const decryptedData = this.#crypto.privateDecrypt({
@@ -171,10 +195,26 @@ class CVK {
         
         // let channelCVK = ElGamal(Alphabet, )
         const encryptedMessage = buileten.buileten;
-        this.#listOfVoters.find((v, i) => {
 
-        })
+        const verification = this.#listOfVoters.find((voterN, i) => {
+            const isVerified = crypto.verify(
+                "sha256",
+                Buffer.from(voterN.name),
+                {
+                    key: buileten.signa.theirPKey,
+                    padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+                },
+                buileten.signa.theirSign
+            )
 
+            if (isVerified) {
+                this.#listOfVoters[i].status = "checked";
+            }
+            // console.log(isVerified);
+            return isVerified;
+        });
+
+        // console.log(verification);
 
         return buileten;
     }
@@ -185,7 +225,41 @@ const data = "Repto Potroshenko";
 let CVK1 = new CVK("CVK#1");
 
 let voter1 = new Voter("Anna Yaroslavna", 990, "CVK#1", "Repto Potroshenko");
-console.log(CVK1.getBuileten(voter1.sendBuileten(CVK1.givePublicKey("voter1"))));
+// let voter2 = new Voter("Somebody", 12, "CVK#1", "Repto Potroshenko");
+
+let voters = [
+    new Voter(),
+    new Voter(),
+    new Voter(),
+    new Voter(),
+    new Voter(),
+    new Voter(),
+]
+// let voter2 = new Voter("Anna Yaroslavna", 990, "CVK#1", "Repto Potroshenko");
+CVK1.getBuileten(voter1.sendBuileten(CVK1.givePublicKey(voter1.name)));
+// CVK1.getBuileten(voter2.sendBuileten(CVK1.givePublicKey(voter2.name)));
+
+
+let statistic = {
+    "Не проголосувало:": 0,
+    "Проголосувало неправильно": 0,
+    "Виборець не має права голосувати": 0,
+    "Виборець хоче проголосувати повторно": 0,
+    "Виборець хоче проголосувати замість іншого виборця": 0,
+};
+
+
+function eVoting(voter, cvk) {
+    const pk = cvk.givePublicKey(voter.name);
+    if (pk === 0) {
+        statistic["Виборець не має права голосувати"]++;
+        return 0;
+    }
+
+    const sendedBuileten = voter.sendBuileten(pk);
+
+
+}
 
 // voter1.givePublicKey();
 // console.log(voter1.sendBuileten());
