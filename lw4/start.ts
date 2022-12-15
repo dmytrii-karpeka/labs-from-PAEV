592f264f692cab3638ad82e58dcd94e03ba6cfcb
dmytrii-karpeka <dmitriy.karpeka@gmail.com>
2022-12-16 00:11:01 +0200

    v 0.2

--- lw4/start.ts
+++ lw4/start.ts
@@ -7,8 +7,10 @@ interface Register {
     randomString: string
 }
 
-interface MessageToA {
-    
+interface messageObject {
+    messages: string,
+    sign: Buffer,
+    key: crypto.KeyObject
 }
 
 class Voter {
@@ -117,26 +119,129 @@ class Voter {
         return this.register;
     }
 
-    // decipherMessage() {
-    //     let testDec: Register = this.register.slice(-1)[0];
-    //     let deciphered: string[] = [];
-    //     testDec.result.forEach((string) => {
-    //         const decryptedData = crypto.privateDecrypt(
-    //             {
-    //               key: this.privateKey,
-    //               // In order to decrypt the data, we need to specify the
-    //               // same hashing function and padding scheme that we used to
-    //               // encrypt the data in the previous step
-    //               padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-    //               oaepHash: "sha256",
-    //             },
-    //             Buffer.from(string)
-    //           );
-    //         deciphered.push(decryptedData.toString());
-    //     })
+    decipherMessagesWithLongKey(messages: (String|Buffer)[][]) {
+        let partDecipheredMessages: (String|Buffer)[][] = [];
+        messages.forEach((message) => {
+            let partDecipheredMessage: (String|Buffer)[] = [];
+            message.forEach((singleString) => {
+                const decryptedString = crypto.privateDecrypt(
+                    {
+                      key: this.longPrivateKey,
+                      // In order to decrypt the data, we need to specify the
+                      // same hashing function and padding scheme that we used to
+                      // encrypt the data in the previous step
+                      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+                      oaepHash: "sha256",
+                    },
+                    Buffer.from(singleString)
+                  );
+                
+                const convertedString = decryptedString.toString();
+                if (convertedString.length !== 5) {
+                    partDecipheredMessage.push(decryptedString);
+                }
+            });
+            partDecipheredMessages.push(partDecipheredMessage);
+        });
+        return partDecipheredMessages;
+    }
 
-        // console.log(deciphered);
-    // }
+    decipherMessagesWithShortKey(messages: (String|Buffer)[][]) {
+        let partDecipheredMessages: (String|Buffer)[][] = [];
+        messages.forEach((message) => {
+            let partDecipheredMessage: (String|Buffer)[] = [];
+            message.forEach((singleString) => {
+                const decryptedString = crypto.privateDecrypt(
+                    {
+                      key: this.shortPrivateKey,
+                      // In order to decrypt the data, we need to specify the
+                      // same hashing function and padding scheme that we used to
+                      // encrypt the data in the previous step
+                      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+                      oaepHash: "sha256",
+                    },
+                    Buffer.from(singleString)
+                  );
+                
+                const convertedString = decryptedString.toString();
+                if (convertedString.length !== 5) {
+                    partDecipheredMessage.push(decryptedString);
+                }
+            });
+            partDecipheredMessages.push(partDecipheredMessage);
+        });
+        return partDecipheredMessages;
+    }
+
+    shuffle(messages: (String|Buffer)[][]) {
+        let currentIndex = messages.length,  randomIndex;
+
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+
+            // Pick a remaining element.
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [messages[currentIndex], messages[randomIndex]] = [
+            messages[randomIndex], messages[currentIndex]];
+        }
+
+        return messages;
+    }
+
+    sign(messages: (String|Buffer)[][]) {
+        let shuffeledMessages = this.shuffle(messages);
+        let stringifiedMessages = JSON.stringify(shuffeledMessages);
+
+        // The signature method takes the data we want to sign, the
+        // hashing algorithm, and the padding scheme, and generates
+        // a signature in the form of bytes
+        const signature = crypto.sign("sha256", Buffer.from(stringifiedMessages), {
+        key: this.longPrivateKey,
+        padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+        });
+
+        let formedObject: messageObject = {
+            messages: stringifiedMessages,
+            sign: signature,
+            key: this.longKey
+        };
+
+        return formedObject;
+    }
+
+    verify(formedObject: messageObject) {
+        let newMessage = [];
+        const isVerified = crypto.verify(
+            "sha256",
+            Buffer.from(formedObject.messages),
+            {
+              key: formedObject.key,
+              padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+            },
+            formedObject.sign
+          );
+
+        if (isVerified) {
+            // console.log("Verified");
+            newMessage = JSON.parse(formedObject.messages);
+            return newMessage;
+        }
+    }
+
+    // TODO
+    findVote(decipheredMessages: (string|Buffer)[][]) {
+        return decipheredMessages.flat().map(message => message.toString()).includes(this.buileten);
+    }
+
+    // TODO
+    countVotes() {
+        ;
+    }
+
+
 }
 
 function main() {
@@ -147,10 +252,10 @@ function main() {
     const basebin = 1024;
 
     let voters: Voter[] = [
-        new Voter("A", "For", basebin*4),
+        new Voter("A", "Support", basebin*4),
         new Voter("B", "Against", basebin*3),
-        new Voter("C", "For", basebin*2),
-        new Voter("D", "For", basebin)
+        new Voter("C", "Support", basebin*2),
+        new Voter("D", "Support", basebin)
     ];
 
     // everybody cipher buileten+rS(random string) with open key
@@ -161,10 +266,7 @@ function main() {
         voter.cipherWithPublicKey(voters[0].getShortPublicKey(), "");
     });
 
-    // voters.forEach((voter: Voter, index) => {
-    //     voter.generateNewPairOfKeys(basebin*(8 - index))
-    // });
-
+    // Second iteration with longer key
     voters.forEach((voter: Voter) => {
         voter.cipherWithPublicKey(voters[3].getLongPublicKey(), voter.generateRandomString(5));
         voter.cipherWithPublicKey(voters[2].getLongPublicKey(), voter.generateRandomString(5));
@@ -172,8 +274,53 @@ function main() {
         voter.cipherWithPublicKey(voters[0].getLongPublicKey(), voter.generateRandomString(5));
     });
 
-    console.log(voters[0].completedMessage(), voters[0].logReg());
+    // Get completed messages from all voters
+    let messages: (String|Buffer)[][] = [];
+    voters.forEach((voter) => {
+        let message = voter.completedMessage();
+        messages.push(message);
+    });
 
+    // Deciphering of messages and deleting random strings
+    let newMessages = messages;
+    voters.forEach((voter) => {
+        let decipheredM = voter.decipherMessagesWithLongKey(newMessages);
+        let shuffledM = voter.shuffle(decipheredM);
+        newMessages = shuffledM;
+    });
+
+    // Deciphering for second time and deleting random strings, validating and signing, deleting signature
+    let checkedM0 = voters[0].decipherMessagesWithShortKey(newMessages);
+    let checkedObject0 = voters[0].sign(checkedM0);
+
+    let validaded1 = voters[1].verify(checkedObject0);
+    let checkedM1 = voters[1].decipherMessagesWithShortKey(validaded1);
+    let checkedObject1 = voters[1].sign(checkedM1);
+
+    let validaded2 = voters[2].verify(checkedObject1);
+    let checkedM2 = voters[2].decipherMessagesWithShortKey(validaded2);
+    let checkedObject2 = voters[2].sign(checkedM2);
+
+    let validaded3 = voters[3].verify(checkedObject2);
+    let checkedM3 = voters[3].decipherMessagesWithShortKey(validaded3);
+    let checkedObject3 = voters[3].sign(checkedM3);
+
+
+    let finalVerified0 = voters[0].verify(checkedObject3);
+    let finalVerified1 = voters[1].verify(checkedObject3);
+    let finalVerified2 = voters[2].verify(checkedObject3);
+    let finalVerified3 = voters[3].verify(checkedObject3);
+    // Check whether everyone agree with final deciphered list of builetens
+    // voters.every(voter => voter.findVote())
+
+    
+
+    // console.log(newMessages[0]);
+    newMessages.forEach((message) => {
+        message.forEach((singleString) => {
+            console.log(singleString.toString());
+        })
+    })
     // test drive
 
 }
